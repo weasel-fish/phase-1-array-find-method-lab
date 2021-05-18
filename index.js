3f411b3942b4b552a74e8b14dd4bd881a4e75a57
github username <github email address>
2021-05-18 16:30:50 -0400

    commit tree b8b3c2a1ab1bca7e233f338ee3e1421f3d6455be

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(record) {
+    const win = record.find(elem => elem.result === 'W');
+    if(win){
+        return win.year;
+    } else {
+        return undefined;
+    }
+    
+}
